b0d830164be6f0d24ded99457dd05e9525931d21
corbie0091 <corbie0091@gmail.com>
2024-11-06 06:04:32 +0900

    이미지 넣는 법, public폴더 이용하기

--- README.md
+++ README.md
@@ -8,3 +8,38 @@
 > [설치] bootstrap은 CSS파일도 넣으라 해서 그것도 넣어보자. ( App.js파일에서 import해오기 혹은 index.html에 <link>태그로 가져오기 )
 > [활용] 버튼을 가져오고 싶다? 사이트에서 button검색 > 버튼 예제중 > 원하는 버튼 코드 복사 > 붙여넣기 > Button은 (대문자) 컴포넌트 이므로 > import 코드 적기 > React App 페이지보면 버튼나와있음 > 동적 UI도 이렇게 생성 가능 > Navbar도 완성 >
 > [참고] 물론 이대로만 쓰는거 아니고 className으로 설정해서 CSS로 커스터마이징도 가능
+
+2강. 이미지 넣는 법 & public 폴더 이용하기
+A.이미지를 불러오자.
+case1] CSS파일에서 src안의 이미지를 넣고싶으면 ? > 그냥 >
+./bg.png 이런식으로 경로 넣어주면된다.
+App.css)
+.main-bg {
+height: 300px;
+background-image: url("./bg.png");
+}
+이후 css설정
+case2] img파일을 만들어서 거기에서 가져오려면 경로 설정 수정필요하다. > ./img/bg.png
+
+case3] import 작명 from "./img/bg.png" 로 불러오고
+이를
+
+<div className="main-bg" style={{ backgroundImage: `url(${bg})` }}></div> 
+이렇게 가져올 수도 있다. js에서 변수를 적용시키려면 역따옴표(``)를 사용해서 ${} 사용하고 그안에 변수를 입력해주자. 
+[참고] 하지만 이렇게 하면
+html에서 src폴더 이미지 100개면 어쩌지? -> import도 100개???
+리액트는 사이트발행전에 html css js 파일을 압축함 (bundling) 하지만 public 폴더 안의 파일은 압축되지 않음
+그래서 html에서 public 폴더>이미지 경로 사용하면 됨
+public폴더 > 이미지 보관이 가능 > /이미지경로 이렇게 사용 ? 
+[주의점] 이렇게 사용하면 서브경로에 발행하고싶을때 문제가 생길 수 있음 => 그래서 경로 수정해줘야함 > create-react-app 라이브러리 공식사이트에서 확인 > 
+<img src={process.env.PUBLIC_URL + "/img/logo.png"}>
+이렇게 사용 ( = 이게 public폴더 이미지 쓰는 권장방식)
+ex) <img
+              src={process.env.PUBLIC_URL + "/col-mg1.jpg"}
+              width="80%"
+              height={"110px"}
+            />
+
+case4] 외부에 호스팅해둔 이미지라면 이미지 절대주소만 넣어줘도 된다.
+B. 상품 레이아웃 3개를 만들어보자. - Bootstrap으로 할 것임
+Bootstrap - grid 활용해도 됨
--- src/App.js
+++ src/App.js
@@ -1,21 +1,54 @@
 import { Container, Nav, Navbar } from "react-bootstrap";
 import "./App.css";
 import Button from "react-bootstrap/Button";
+import bg from "./img/bg.png";
 
 function App() {
   return (
     <div className="App">
-      <Button variant="primary">Primary</Button>{" "}
       <Navbar bg="dark" data-bs-theme="dark">
         <Container>
-          <Navbar.Brand href="#home">Navbar</Navbar.Brand>
+          <Navbar.Brand href="#home">OttShop</Navbar.Brand>
           <Nav className="me-auto">
-            <Nav.Link href="#home">Home</Nav.Link>
-            <Nav.Link href="#features">Features</Nav.Link>
-            <Nav.Link href="#pricing">Pricing</Nav.Link>
+            <Nav.Link href="#shop">SHOP</Nav.Link>
+            <Nav.Link href="#about">ABOUT</Nav.Link>
+            <Nav.Link href="#community">COMMUNITY</Nav.Link>
+            <Nav.Link href="#Cart">CART</Nav.Link>
           </Nav>
         </Container>
       </Navbar>
+      <div className="main-bg" style={{ backgroundImage: `url(${bg})` }}></div>
+      <div className="container">
+        <div className="row">
+          <div className="col-md-4">
+            <img
+              src="https://economist.co.kr/data/ecn/image/2023/02/04/ecn20230204000012.jpg"
+              width="80%"
+            />
+            <h4>상품명</h4>
+            <p>상품설명</p>
+          </div>
+          <div className="col-md-4">
+            <img
+              src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTj6cuNo1jEkqmkDFuYfLvMT5DWUYg4FDE2Gg&s"
+              width="80%"
+            />
+            <h4>상품명</h4>
+            <p>상품설명</p>
+          </div>
+          <div className="col-md-4">
+            <img
+              src={process.env.PUBLIC_URL + "/col-mg1.jpg"}
+              width="80%"
+              height={"110px"}
+            />
+            <h4>상품명</h4>
+            <p>상품설명</p>
+          </div>
+        </div>
+      </div>
+      <br />
+      <Button variant="primary">구매하기</Button>{" "}
     </div>
   );
 }
